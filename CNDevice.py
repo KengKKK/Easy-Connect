176884abb02ac3f6b548dc23c8b2087d84828489
KengKKK <kengkingzaa@gmail.com>
2018-11-12 23:52:28 +0700

    add device test

---
+++ CNDevice.py
@@ -0,0 +1,28 @@
+import paramiko
+import time
+
+time.sleep(1)
+
+ip = "10.x.x.x"
+host = ip
+username = "myusername"
+password = "mypassword"
+
+remote_conn_pre = paramiko.SSHClient()
+remote_conn_pre
+remote_conn_pre.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+remote_conn_pre.connect(ip, username=username, password=password, look_for_keys=False, allow_agent=False)
+print("SSH connection established to " + host)
+    
+remote_conn = remote_conn_pre.invoke_shell()
+print("Interactive SSH session established")
+
+output = remote_conn.recv(1000)
+print(output)   
+
+remote_conn.send("\n")
+remote_conn.send("utils system restart\n")
+time.sleep(5)
+
+output = remote_conn.recv(5000)
+print(output)
