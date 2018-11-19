4707efae3ea0e50373c83b38f8ba1c73db0f3a1f
KengKKK <kengkingzaa@gmail.com>
2018-11-19 13:08:49 +0700

    test cmd

--- connect.py
+++ connect.py
@@ -1,9 +1,30 @@
 #!/usr/bin/env python
+import paramiko
 import os
 import sys
 
-from subprocess import call
-call(["ls", "-l"])
+import subprocess
+import cmd
+import time
+import datetime
+import getpass
+
+
+
 
 os.system("echo 'hello shell'")
-os.system("some_command with args")
+os.system("echo 'eiei'")
+
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+host = '9.10.11.12'
+port, user, password = 22, 'usr', 'pass'
+ssh.connect(host, port,  user, password)
+stdin,stdout,stderr = ssh.exec_command("cd /opt/app && ./app-tool some_command")
+
+
+for line in iter(stdout.readline,""): 
+    print(line)
+
+ssh.close()
